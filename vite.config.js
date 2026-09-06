ba94a28ede6107608297ae455bce7f70f9bdbe94
Tiagomelissa27 <tiagoandersongfk@gmail.com>
2026-09-06 15:12:13 -0300

    alterações de page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/Bingo/', // IMPORTANTE: Define a rota base para o deploy no GitHub Pages
 })
