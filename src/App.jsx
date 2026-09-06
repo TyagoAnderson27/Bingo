93c326bb488635b1ca5a7e772a0eb45238079e68
Tiagomelissa27 <tiagoandersongfk@gmail.com>
2026-09-06 15:58:18 -0300

    alteração de código

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Bingo from './Bingo.jsx';
 
 function App() {
   return (
-    <div style={{ minHeight: '100vh', backgroundColor: '#f1f5f9', padding: '20px' }}>
+    <div style={{ minHeight: '100vh' }}>
       <Bingo />
     </div>
   );
